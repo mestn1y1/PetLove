7bc09be42fdf87df70a35267fc447473195f9556
Serhii Tyron <mestn1y@hotmail.com>
2025-03-08 18:31:27 +0200

    add markup for not found page/mob screen

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,11 @@
-import css from "./NotFoundPage.module.css"; export default function
-NotFoundPage
-() { return (
-<div>
-  NotFoundPage
-  Component</div>
-
-); };
+import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
+export default function NotFoundPage() {
+  return (
+    <section className={css.container}>
+      <h1 className={css.title}>4<span className={css.titleImg}></span>4</h1>
+      <p className={css.description}>Ooops! This page not found :( </p>
+      <Link to="/home" className={css.link}>To home page</Link>
+    </section>
+  );
+}
