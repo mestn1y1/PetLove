b0d5dca2f170d119a1fd0ccc6664a8ca806fabd9
Serhii Tyron <mestn1y@hotmail.com>
2025-03-12 00:20:28 +0200

    adding modal attention

--- src/components/Modals/ModalAttention/ModalAttention.jsx
+++ src/components/Modals/ModalAttention/ModalAttention.jsx
@@ -1,8 +1,31 @@
-import css from "./ModalAttention.module.css"; export default function
-ModalAttention
-() { return (
-<div>
-  ModalAttention
-  Component</div>
+import { Icons } from "../../Icons/Icons";
+import { NavLink } from "react-router-dom";
+import css from "./ModalAttention.module.css";
+export default function ModalAttention({ handleClose }) {
+  return (
+    <div className={css.modalContainer}>
+      <button className={css.bloseBtn} onClick={handleClose}>
+        <Icons iconName="close" className={css.iconClose} />
+      </button>
+      <div className={css.imgWrap}>
+        <img src="/images/dog_mod.png" alt="dog" className={css.imgDog} />
+      </div>
+      <h2 className={css.tittleAttention}>Attention</h2>
+      <p className={css.description}>
+        We would like to remind you that certain functionality is available only
+        to authorized users.If you have an account, please log in with your
+        credentials. If you do not already have an account, you must register to
+        access these features.
+      </p>
+      <div className={css.authNav}>
+        <NavLink to="/login" className={css.link}>
+          Log In
+        </NavLink>
 
-); };
+        <NavLink to="/register" className={css.link}>
+          Register
+        </NavLink>
+      </div>
+    </div>
+  );
+}
--- src/pages/FriendsPage/FriendsPage.jsx
+++ src/pages/FriendsPage/FriendsPage.jsx
@@ -1,5 +1,23 @@
+import ModalAttention from "../../components/Modals/ModalAttention/ModalAttention";
+import { ModalWrap } from "../../components/Modals/ModalWrap/ModalWrap";
 import Title from "../../components/Title/Title";
+import { useState } from "react";
 import css from "./FriendsPage.module.css";
+
 export default function FriendsPage() {
-  return <Title text="Our friends" />;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div>
+      <Title text="Our friends" />
+      <button onClick={openModal}>Open Modal</button>
+
+      <ModalWrap isOpen={isModalOpen} handleClose={closeModal}>
+        <ModalAttention handleClose={closeModal} />
+      </ModalWrap>
+    </div>
+  );
 }
