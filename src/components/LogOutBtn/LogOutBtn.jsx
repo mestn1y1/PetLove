b5d25fa28f853db129f48b3380301cbd1091fc2b
Serhii Tyron <mestn1y@hotmail.com>
2025-03-11 23:23:48 +0200

    adding navigate for log out btn

--- src/components/LogOutBtn/LogOutBtn.jsx
+++ src/components/LogOutBtn/LogOutBtn.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-
+import { useNavigate } from "react-router-dom"; // для редиректу на Home page
 import { logOut } from "../../redux/auth/operations";
 import { ModalWrap } from "../Modals/ModalWrap/ModalWrap";
 import { Button } from "../Button/Button";
@@ -10,11 +10,17 @@ import css from "./LogOutBtn.module.css";
 export default function LogOutBtn({ isHome }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  const handleLogout = () => {
-    dispatch(logOut());
+  const handleLogout = async () => {
+    try {
+      await dispatch(logOut());
+
+      navigate("/home");
+    } catch (error) {}
     closeModal();
   };
 
